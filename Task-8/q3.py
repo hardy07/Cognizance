0aee54d440aa1e6787600e015cbcfd2387253b7b
hardy07 <63062063+hardy07@users.noreply.github.com>
2022-03-09 18:34:44 +0530

    Update and rename Q3.py to q3.py

---
+++ Task-8/q3.py
@@ -0,0 +1,9 @@
+#question3
+
+import numpy as np #imported numpy to make this program work
+
+print(0 * np.nan)
+print(np.nan != np.nan)
+print(np.inf > np.nan)
+print(np.nan - np.nan)
+print(0.3 == 3 * 0.1)
