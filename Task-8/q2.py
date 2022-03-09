e3bbbde100b328260ca4e8129c1e9f900887542c
hardy07 <63062063+hardy07@users.noreply.github.com>
2022-03-09 19:53:47 +0530

    Update q2.py

--- Task-8/q2.py
+++ Task-8/q2.py
@@ -1,1 +1,11 @@
+#question2
 
+import numpy as np #importing numpy
+x = np.random.randint(1,8,6) #creating a random array as x and printing it
+print("First array:\n", x)
+
+y = np.random.randint(0,7,6) #creating a random array as y and printing it
+print("Second array:\n", y)
+
+equalornahh = np.allclose(x, y) #allclose() will check if both the arrays have similar elements are not and prints it as True or False
+print(equalornahh)
