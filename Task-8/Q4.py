814bf4d40658e13685db72fb63d8432e7b20af6f
hardy07 <63062063+hardy07@users.noreply.github.com>
2022-03-09 18:27:51 +0530

    Update Q4.py

--- Task-8/Q4.py
+++ Task-8/Q4.py
@@ -1,3 +1,5 @@
-import pandas as pd #importing pandas
+import pandas as pd # importing pandas
 ser = pd.Series(['amrita', 'school', 'of', 'engineering', 'chennai' , 'campus'])
-print(ser.str.capitalize()) #helps to convert the very first letter of given series in capital
+print(ser) #before capitalizing
+print(ser.str.capitalize()) #After capitalizing
+#Series.str.capitalize() helps to convert the very first letter of given series in capital
