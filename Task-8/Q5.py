db4432dad872373d45806a5a7683f45c809a2727
hardy07 <63062063+hardy07@users.noreply.github.com>
2022-03-09 19:33:55 +0530

    Update Q5.py

--- Task-8/Q5.py
+++ Task-8/Q5.py
@@ -1,1 +1,20 @@
+#2. Multiplying a matrix using numpy
+import numpy as np  #importing numpy
+first=np.array([[1,2,3],[4,5,6],[7,8,9]])  #first matrix
+print("First matrix:")
+print(first)
+secnd=np.array([[4,2,0],[9,1,1],[6,9,6]])  #second matrix
+print("Second Matrix:")
+print(secnd)
+res=np.multiply(first,secnd)  #used np.multiply() to multiply both the arrays 
+print("Multiplication of the two matrices are:")
+print(res)
 
+#5. Array re-dimensioning using numpy
+import numpy as np
+a = np.array([9, 1, 1, 4, 2, 0, 6, 9, 6, 1, 2, 3])
+print("Before re-dimensioning:")
+print(a)
+newa = a.reshape(4, 3) #reshape() gives a new shape to an array
+print("After re-dimensioning:")
+print(newa)
