bc19c1d9dac3965a1d7e4bfe99fc24f3dddd938d
hardy07 <63062063+hardy07@users.noreply.github.com>
2022-03-09 18:43:05 +0530

    Update q3.py

--- Task-8/q3.py
+++ Task-8/q3.py
@@ -2,8 +2,8 @@
 
 import numpy as np #imported numpy to make this program work
 
-print(0 * np.nan)
-print(np.nan != np.nan)
-print(np.inf > np.nan)
+print(0 * np.nan) #NaN stands for Not A Number
+print(np.nan != np.nan) 
+print(np.inf > np.nan) #inf refers infinity
 print(np.nan - np.nan)
 print(0.3 == 3 * 0.1)
